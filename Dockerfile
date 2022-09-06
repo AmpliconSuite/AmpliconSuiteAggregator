40f4c950fb2bc0950c83c1a7aec2e69056f13505
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2022-09-06 13:40:40 -0700

    updates to aggregator

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,29 @@ FROM ubuntu:20.04
 RUN mkdir /opt/genepatt && chmod a+rwx /opt/genepatt
 RUN mkdir /opt/genepatt/extracted
 RUN mkdir /opt/genepatt/aggregated
-WORKDIR /opt/genepatt
+RUN mkdir /opt/genepatt/programs
+RUN mkdir /opt/genepatt/files
+#COPY libs/AmpliconClassifier-main.zip /opt/genepatt/programs
+
+
 
 
 ## Install required packages
 RUN apt-get update
 RUN apt install -y --fix-missing python3.8
 RUN apt-get install -y --fix-missing python3-pip
+RUN apt-get install -y unzip wget
+
+
+
+WORKDIR /opt/genepatt
+
 
 RUN pip3 install pandas
 
 COPY src* /opt/genepatt
+
+## for testing purposes
+#RUN mkdir /opt/genepatt/testzips
+#COPY testzips/* /opt/genepatt/testzips
+#COPY files.txt /opt/genepatt
--- src/AmpliconSuiteAggregator.py
+++ src/AmpliconSuiteAggregator.py
@@ -11,16 +11,23 @@ import re
 import pandas as pd
 import tarfile
 import shutil
+import json
+import subprocess
+
+
 
 
 class Aggregator():
-    def __init__(self, filelist, root):
+    def __init__(self, filelist, root, output_name):
         self.filelist_fp = filelist
         self.root = root
-        self.get_zip_paths()
+        self.output_name = output_name
+        print(self.get_zip_paths())
         self.unzip()
-        self.aggregate()
+        self.aggregate_tables()
+        self.move_files()
         self.cleanup()
+
 
 
     def get_zip_paths(self):
@@ -47,6 +54,8 @@ class Aggregator():
         returns -- None
         """
 
+        # os.mkdir('all_AA_results')
+
         for zip_fp in self.zip_paths:
             fp = os.path.join(self.root, zip_fp)
             try:
@@ -68,33 +77,79 @@ class Aggregator():
                     sample_name = dir_name.replace("_cnvkit_output", "")
                     sample_name = sample_name.replace('extracted/', "")
                     samples.append(sample_name)
-
         self.samples = samples
         print(self.samples)
 
 
 
-    def aggregate(self):
+    def aggregate_tables(self):
         """
         From the unzipped paths, start aggregating results
 
         """
-        aggregate = pd.DataFrame(columns = ["Sample name", "AA amplicon number", "Feature ID", "Classification", "Location", "Oncogenes",
-                   "Complexity score", "Feature BED file", "AA PNG file", "AA PDF file"])
 
-
+        output_head = ["Sample name", "AA amplicon number", "Feature ID", "Classification", "Location", "Oncogenes",
+                           "Complexity score",
+                           "Captured interval length", "Feature median copy number", "Feature maximum copy number",
+                           "Reference version", "Feature BED file", "CNV BED file", "AA PNG file", "AA PDF file",
+                           "Run metadata JSON"]
+        output_table_lines = [output_head, ]
+        aggregate = pd.DataFrame(columns = output_head)
+        cycle_files = []
+        graph_files = []
+        runs = {}
+        sample_num = 1
         # aggregate results
         for root, dirs, files in os.walk("extracted", topdown = False):
             for name in files:
                 if "_result_table.tsv" in name:
                     result_table_fp = os.path.join(root, name)
-                    print(result_table_fp)
                     df = pd.read_csv(result_table_fp, delimiter = '\t')
                     aggregate = pd.concat([aggregate, df], ignore_index = True)
-                    print(os.path.join(root, name))
+
+                    ## convert to json
+                    runs[f'sample_{sample_num}'] = json.loads(df.to_json(orient = 'records'))
+                    sample_num += 1
+
+            for dir in dirs:
+                if "files" == dir:
+                    dir_fp = os.path.join(root, dir)
+                    os.system(f'cp {dir_fp}/* /opt/genepatt/files')
+
+
+
         ## output the table
         aggregate.to_csv('aggregated_results.csv')
         aggregate.to_html('aggregated_results.html')
+        with open('run.json', 'w') as run_file:
+            json.dump({'runs': runs}, run_file)
+        run_file.close()
+
+    def aggregate_json(self, json_files):
+        """
+        Aggregates json files from separate samples
+
+        """
+        out_dict = {'run' : {}}
+        sample_ct = 1
+        for fp in json_files:
+            sample_dict = json.load(open(fp))
+            out_dict['run'][f'sample_{sample_ct}'] = sample_dict
+            sample_ct += 1
+
+        with open('run.json', 'w') as out_json:
+            json.dump(out_dict, out_json)
+        out_json.close()
+
+    def move_files(self):
+        """
+        Move files to correct location for output
+        """
+
+        shutil.move('/opt/genepatt/files', '/opt/genepatt/extracted/files')
+        shutil.copy('run.json', '/opt/genepatt/extracted/run.json')
+        shutil.copy('aggregated_results.csv', '/opt/genepatt/extracted/aggregated_results.csv')
+        shutil.copy('aggregated_results.html', '/opt/genepatt/extracted/aggregated_results.html')
 
 
     def tardir(self, path, tar_name):
@@ -105,25 +160,33 @@ class Aggregator():
             for root, dirs, files in os.walk(path):
                 for file in files:
                     tar_handle.add(os.path.join(root, file))
+        tar_handle.close()
 
     def cleanup(self):
         """
         Zips the aggregate results, and deletes files for cleanup
 
         """
-
-        self.tardir('./extracted', 'aggregated.tar.gz')
-
-        shutil.rmtree('./extracted')
+        self.tardir('extracted', f'{self.output_name}.tar.gz')
+        shutil.rmtree('extracted')
 
 
 
 if __name__ == "__main__":
+    # os.environ["AA_DATA_REPO"] = os.path.join('/opt/genepatt', '.data_repo')
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-flist", "--filelist", type = str,
             help = "List of zip files to use")
+
+    parser.add_argument("--output_name", type = str, help = "Output Prefix")
+
     args = parser.parse_args()
+
+
+    # os.system(f'bash download_ref.sh {REFERENCE}')
+
 
     filelist = args.filelist
     root = '.'
-    aggregate = Aggregator(filelist, root)
+    aggregate = Aggregator(filelist, root, args.output_name)

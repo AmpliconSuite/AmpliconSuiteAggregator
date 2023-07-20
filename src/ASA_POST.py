0562820874fcabc9094dfc7522285d52eda2bdd0
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2023-07-20 11:34:25 -0700

    Update ASA_POST.py

--- src/ASA_POST.py
+++ src/ASA_POST.py
@@ -14,7 +14,7 @@ def post_package(fp, data, user):
             'private': boolean
             'project_members': list}
     '''
-    homepage = 'https://ampliconrepository.org/'
+    homepage = 'https://ampliconrepository.org'
     session = requests.Session()
     cookie = session.get(homepage)
     csrf_token = cookie.cookies.get_dict()['csrftoken']
@@ -37,7 +37,7 @@ def post_package(fp, data, user):
 #     ## to do one aggregation
 #     # python3 /module/src/AmpliconSuiteAggregator.py -flist /module/gpunit/inputs/input_list.txt
 
-#     homepage = 'https://dev.ampliconrepository.org'
+#     homepage = 'https://ampliconrepository.org'
 #     ## get cookie first
 #     session = requests.Session()
 #     cookie = session.get(homepage)
@@ -54,10 +54,11 @@ def post_package(fp, data, user):
 #         'X-CSRFToken':csrf_token,
 #     }
     
-#     data = {'project_name': 'testPOST_Private_Project_with_s3adjustment_todev',
+#     data = {'project_name': 'testPOST_Private_Project_with_s3adjustment_to_prod',
 #             'description':'a test POST request to API',
 #             'private':True,
 #             'project_members':['edh021@ucsd.edu']}
+    
 #     files = {'file': open(upload_file, 'rb')}
 
 #     response = requests.post(upload_url,data = data, files = files)
